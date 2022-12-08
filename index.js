8ee897739a5ff3c00a9dc5382964ad780ffce464
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2022-12-07 23:42:57 -0600

    New sql postgres base

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const port = 4000;
+ 
+const app = express();
+ 
+app.use(helmet());
+app.use(express.json())
+ 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+ 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
